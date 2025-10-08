63e4efa8c8168d78f9e64885052195aaa2a91c50
stephieOj <quilamana@outlook.com>
2025-10-08 16:26:58 +0100

    Setup dictionary component-search form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
+import Dictionary from './Dictionary.jsx'
 
 export default function App() {
 
   return (
     <div className= "App">
       <div className="container">
-      <h1> Hello World from React</h1>
+        <header className="App-header">
+          <h1>Dictionary</h1>
+          <h2>What word do you want to look up?</h2>
+        </header>
+
+        <main>
+          <Dictionary />
+        </main>
+
 
     <footer> This project was coded by <a 
         href="https://github.com/StephieOj" target="_blank"rel='noopener noreferrer'> <strong>Stephie Oj 💕</strong></a>  and is <a
