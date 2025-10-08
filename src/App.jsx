faf09c6167eac2d43abffd26247ea449d36d9459
stephieOj <quilamana@outlook.com>
2025-10-08 15:56:14 +0100

    Created footer and applied styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ export default function App() {
 
   return (
     <div className= "App">
+      <div className="container">
       <h1> Hello World from React</h1>
+
+    <footer> This project was coded by <a 
+        href="https://github.com/StephieOj" target="_blank"rel='noopener noreferrer'> <strong>Stephie Oj 💕</strong></a>  and is <a
+        href="https://github.com/StephieOj/react-dictionary" target="_blank"rel='noopener noreferrer'> <strong>open-sourced on GitHub</strong> </a> and is hosted on <a 
+        href="https://stephieoj-react-dictionary.netlify.app/" target="_blank" rel='noopener noreferrer'> <strong>Netlify</strong></a>
+        <br />
+        <br />
+        <img className="stephie" src="src/assets/stephieoj.png" alt="stephieoj" height="80px" width="80px" />
+    </footer>
+
     </div>
-       
+  </div>   
   )
 }
 
