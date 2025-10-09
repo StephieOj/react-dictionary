9f06b702e02c6d67c7acacb040837bc87989f07f
stephieOj <quilamana@outlook.com>
2025-10-09 19:01:26 +0100

    update img

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
         href="https://stephieoj-react-dictionary.netlify.app/" target="_blank" rel='noopener noreferrer'> <strong>Netlify</strong></a>
         <br />
         <br />
-        <img className="stephie" src="src/assets/stephieoj.png" alt="stephieoj" height="80px" width="80px" />
+        <img className="stephie" src="/src/assets/stephieoj.png" alt="stephieoj" height="80px" width="80px" />
     </footer>
 
     </div>
